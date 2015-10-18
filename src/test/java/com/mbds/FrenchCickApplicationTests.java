0a6c46a9cfd6f47f9d1a134b6c408823965ebdf4
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2015-10-18 16:23:15 +0200

    add class

--- src/test/java/com/mbds/FrenchCickApplicationTests.java
+++ src/test/java/com/mbds/FrenchCickApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = FrenchCickApplication.class)
+@SpringApplicationConfiguration(classes = FrenchChicApplication.class)
 @WebAppConfiguration
 public class FrenchCickApplicationTests {
 
