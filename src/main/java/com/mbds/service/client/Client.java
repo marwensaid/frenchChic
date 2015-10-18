cc89541a1a8ccad9d864b95644df27bc545808bd
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2015-10-18 17:47:59 +0200

    implement Client.java

--- src/main/java/com/mbds/service/client/Client.java
+++ src/main/java/com/mbds/service/client/Client.java
@@ -11,4 +11,77 @@ package com.mbds.service.client;
  */
 public class Client {
     
+    private String nom;
+    private String prenom;
+    private String login;
+    private String motdepasse;
+    private String adressedeFacturation;
+    private String adressedeLivraison;
+    private String numero;
+    
+    public Client findbyLogin(String login, String motdepasse){
+        return null;
+    }
+    
+    public void creer(){}
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setMotdepasse(String motdepasse) {
+        this.motdepasse = motdepasse;
+    }
+
+    public void setAdressedeFacturation(String adressedeFacturation) {
+        this.adressedeFacturation = adressedeFacturation;
+    }
+
+    public void setAdressedeLivraison(String adressedeLivraison) {
+        this.adressedeLivraison = adressedeLivraison;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getMotdepasse() {
+        return motdepasse;
+    }
+
+    public String getAdressedeFacturation() {
+        return adressedeFacturation;
+    }
+
+    public String getAdressedeLivraison() {
+        return adressedeLivraison;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+    
+    
+    
+    
 }
